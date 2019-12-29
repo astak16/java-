4b3bb2c7c9895676095e90dc3c1149f0ca06d8a7
tiantian <1500846601@qq.com>
2019-12-30 00:06:20 +0800

    瀑布流组件初步实现

--- 风袖/components/spu-preview/index.js
+++ 风袖/components/spu-preview/index.js
@@ -1,23 +1,33 @@
 // components/spu-preview/index.js
+import array from "../../miniprogram_npm/lin-ui/common/async-validator/validator/array";
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    data:Object
+    data: Object
   },
-
+  observers: {
+    data(data) {
+      if (!data) return
+      if (!data.tags) return
+      const tags = data.tags.split("$")
+      this.setData({
+        tags
+      })
+    }
+  },
+  
   /**
    * 组件的初始数据
    */
   data: {
-
+    tags: Array
   },
-
+  
   /**
    * 组件的方法列表
    */
-  methods: {
-
-  }
+  methods: {}
 })
