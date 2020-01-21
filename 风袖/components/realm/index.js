004d0cd3ed4aafa17473ce8b342162831e7581ec
tiantian <1500846601@qq.com>
2020-01-21 19:26:44 +0800

    引入购物车

--- 风袖/components/realm/index.js
+++ 风袖/components/realm/index.js
@@ -3,6 +3,7 @@ import {FenceGroup} from "../models/fence-group";
 import {Judger} from "../models/judger";
 import {Spu} from "../../models/spu";
 import {Cell} from "../models/cell";
+import {Cart} from "../../models/cart";
 
 Component({
   /**
@@ -34,7 +35,9 @@ Component({
     noSpec: Boolean,
     skuIntact: Boolean,
     currentValues: String,
-    missingKeys: String
+    missingKeys: String,
+    outStock: Boolean,
+    currentSkuCount: Cart.SKU_MIN_COUNT
   },
   
   /**
@@ -46,6 +49,7 @@ Component({
         noSpec: true
       })
       this.bindSkuData(spu.sku_list[0])
+      this.setStockStatus(spu.sku_list[0].stock, this.data.currentSkuCount)
     },
     processHasSpec(spu) {
       const fenceGroup = new FenceGroup(spu)
@@ -54,6 +58,7 @@ Component({
       const defaultSku = fenceGroup.getDefaultSku()
       if (defaultSku) {
         this.bindSkuData(defaultSku)
+        this.setStockStatus(defaultSku.stock, this.data.currentSkuCount)
       } else {
         this.bindSpuData()
       }
@@ -90,6 +95,23 @@ Component({
         fences: fenceGroup.fences,
       })
     },
+    setStockStatus(stock, currentCount) {
+      this.setData({
+        outStock: this.isOutOfStock(stock, currentCount)
+      })
+    },
+    isOutOfStock(stock, currentCount) {
+      return stock < currentCount
+    },
+    onSelectCount(event) {
+      const currentCount = event.detail.count
+      this.data.currentSkuCount = currentCount
+      
+      if (this.data.judger.isSkuIntact()) {
+        const sku = this.data.judger.getDeterminateSku()
+        this.setStockStatus(sku.stock, currentCount)
+      }
+    },
     onCellTap(event) {
       const data = event.detail.cell
       const {x, y} = event.detail
@@ -102,6 +124,7 @@ Component({
       if (skuIntact) {
         const currentSku = judger.getDeterminateSku()
         this.bindSkuData(currentSku)
+        this.setStockStatus(currentSku.stock, this.data.currentSkuCount)
       }
       this.bindTipData()
       this.bindFenceGroupData(judger.fenceGroup)
